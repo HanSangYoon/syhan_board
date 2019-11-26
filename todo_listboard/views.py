36e96166012a1e7d248d08e6310c38c867b9f0b7
microscope83@gmail.com <microscope83@gmail.com>
2019-11-26 13:37:49 +0900

    todo_listboard

--- todo_listboard/views.py
+++ todo_listboard/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import TodoList
+#from .forms import TodoForm
 # Create your views here.
+
+from django.views import View
+from django.views import generic
+
+class Todo_listBoard(generic.TemplateView):
+    def get(self, request, *args, **kwargs):
+
+        template_name = 'todo_board/todo_list.html'
+        todo_list = TodoList.objects.all()
+
+        return render(request, template_name, {"todo_list": todo_list})
